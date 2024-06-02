15422da8d3fc76c3c74456ccee93d3e8c50419fa
Atharva Chiplunkar <73574922+AtharvaChiplunkar12@users.noreply.github.com>
2024-06-02 17:53:41 -0400

    created type and genre checkbox

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -3,23 +3,33 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import { fetchGenresData } from './components/api';
-import { Chip } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
 
 function ChatBot () {
     const [genres, setGenres] = useState([]);
     const [selectedGenres, setSelectedGenres] = useState([]);
+    const [type, setType] = useState("");
 
-    const type = 'movie';
-
-    const handleAdd = (genre) => {
-		setSelectedGenres([...selectedGenres, genre]);
-		setGenres(genres.filter((g) => g.id !== genre.id));
+    const handleCheckboxChange = (event, name) => {
+        const { checked } = event.target;
+        if (checked) {
+          setSelectedGenres([...selectedGenres, name]);
+        } else {
+          setSelectedGenres(selectedGenres.filter((genre) => genre !== name));
+        }
 	};
 
+    const handleDropDownChange = (e) => {
+        setType(e.target.value);
+    };
+
+    
     const fetchGenres = async () => {
 		const data = await fetchGenresData(type);
 		setGenres(data.genres);
 	};
+
     useEffect(() => {
 		fetchGenres();
 		return () => {
@@ -27,15 +37,47 @@ function ChatBot () {
 		};
 	}, []);
 
+    useEffect(() => {
+        console.log('Selected Genres:', selectedGenres);
+      }, [selectedGenres]);
+
+      useEffect(() => {
+        fetchGenres(type);
+        setSelectedGenres([]);
+      }, [type]);
+
   return (
-    <FormGroup row='true'>
-        {genres &&
-				genres.map((genre) => (
-                    <FormControlLabel control={<Checkbox color="success" />} label={genre.name} />
-					
-				))}
-       
-    </FormGroup>
+    <div>
+    <div>
+        <FormControl fullWidth>
+            <InputLabel>Type</InputLabel>
+            <Select label="Type" onChange={handleDropDownChange}>
+                <MenuItem value="movie">Movies</MenuItem>
+                <MenuItem value="tv">TV Shows</MenuItem>
+            </Select>
+        </FormControl>
+        
+    </div>
+    <div>
+        {type != "" && (
+        <FormGroup row='true'>
+            {genres &&
+            genres.map((genre) => (
+                <FormControlLabel 
+                control={<Checkbox color="success" />} 
+                checked={selectedGenres.includes(genre.name)}
+                onChange={(event)=>handleCheckboxChange(event, genre.name)}
+                label={genre.name} 
+                />
+                
+            ))}
+        </FormGroup>
+)}
+    </div>
+    <div>
+
+    </div>
+</div>
   )
 }
 
