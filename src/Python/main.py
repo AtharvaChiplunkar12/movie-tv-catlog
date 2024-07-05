31c0167dac7c555ab8234afd5bf4d6dc0e3276c5
Atharva Chiplunkar <73574922+AtharvaChiplunkar12@users.noreply.github.com>
2024-07-05 15:10:51 -0400

    test embeddings

--- src/Python/main.py
+++ src/Python/main.py
@@ -6,6 +6,8 @@ from fastapi.middleware.cors import CORSMiddleware
 #from dotenv import load_dotenv
 from key import huggingface_access_key
 import os
+from langchain.prompts import FewShotPromptTemplate
+
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = huggingface_access_key
 
 
@@ -54,61 +56,16 @@ app.add_middleware(
 #     return result1, result2
 
 def read_root(query):
-    prompt_template_name = PromptTemplate(
-    input_variables=['query'],
-    template= f"""
-    Genre Table - 
-    "id": 28, "name": "Action" ,
-    "id": 12, "name": "Adventure" ,
-    "id": 16, "name": "Animation" ,
-    "id": 35, "name": "Comedy" ,
-    "id": 80, "name": "Crime" ,
-    "id": 99, "name": "Documentary" ,
-    "id": 18, "name": "Drama" ,
-    "id": 10751, "name": "Family" ,
-    "id": 14, "name": "Fantasy" ,
-    "id": 36, "name": "History" ,
-    "id": 27, "name": "Horror" ,
-    "id": 10402, "name": "Music" ,
-    "id": 9648, "name": "Mystery" ,
-    "id": 10749, "name": "Romance" ,
-    "id": 878, "name": "Science Fiction" ,
-    "id": 10770, "name": "TV Movie" ,
-    "id": 53, "name": "Thriller" ,
-    "id": 10752, "name": "War" ,
-    "id": 37, "name": "Western"
-    
-    some attribute examples-
-    region: Example: 'US', 'IN'
-    year: Example: 2023, 2024
-    release_date.gte: Example: 2015-01-01
-    release_date.lte: Example: 2015-01-01
-    with_genres: Example: 23, 28 
-    with_origin_country: Example: 'US', 'IN'
-    without_genres: Example: 18, 99 
-    with_original_language: Example: 'hi', 'en'
-
-Result format -
-  "year": ,
-  "region": "",
-  "release_date.gte": "",
-  "release_date.lte": "",
-  "vote_average.gte": ,
-  "vote_average.lte": ,
-  "with_genres": "", (look at the genre table a fill the values)
-  "with_origin_country": "",
-  "without_genres": "", (look at the genre table a fill the values)
-  "with_original_language": ""
-  This is required fields in the object.
-From this query: {query}
-Extract all the details and fill in the JSON object accordingly. no white spaces
-"""
-
+    few_shot_prompt = FewShotPromptTemplate(
+    example_selector=example_selectors,
+    example_prompt=example_prompt,
+    input_variables= ["query"],
+    suffix= template,
     )
-    chain1 = LLMChain(llm=llm, prompt=prompt_template_name)
+    chain = LLMChain(llm=llm, prompt=few_shot_prompt)
 
-    result1 = chain1.run(query=query)
-    
+    result1 = chain.run(query)
+    print(result1)
     return result1
 
-print(read_root("horror comedy bollywood movies"))
+#print(read_root("horror movies"))
